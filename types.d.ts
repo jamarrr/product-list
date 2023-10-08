0c95cea48f751f54c507cab7aa77282727c096c4
John Mar Abarientos <johnmaraabarientos@gmail.com>
2023-10-09 00:50:36 +0800

    Done with API integration

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+export type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+export type TableProps = {
+  headers: string[];
+  data: Product[];
+  setSelectedProduct: (product: Product) => void;
+};
